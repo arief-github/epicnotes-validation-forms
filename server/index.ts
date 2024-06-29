6a436b889881dc536a5df432fdebfe4ed6f1771a
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2024-06-29 23:09:21 +0700

    feat: basic setup rate Limiting in express backend

--- server/index.ts
+++ server/index.ts
@@ -8,6 +8,7 @@ import chalk from 'chalk'
 import closeWithGrace from 'close-with-grace'
 import compression from 'compression'
 import express from 'express'
+import rateLimit from 'express-rate-limit'
 import getPort, { portNumbers } from 'get-port'
 import morgan from 'morgan'
 
@@ -70,6 +71,18 @@ app.use(express.static('public', { maxAge: '1h' }))
 
 morgan.token('url', req => decodeURIComponent(req.url ?? ''))
 app.use(morgan('tiny'))
+
+const minimumRateLimit = 5;
+// const limitMultiple = process.env.TESTING ? 10_000 : 1
+
+app.use(
+	rateLimit({
+		windowMs: 60 * 1000,
+		limit: minimumRateLimit,
+		standardHeaders: true,
+		legacyHeaders: false,
+	})
+)
 
 app.use((_, res, next) => {
 	res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
